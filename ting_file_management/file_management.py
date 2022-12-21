553380667ee75333096f065c4402f30a0d2a39b1
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-12-21 18:17:45 -0300

    feat: Implementa a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt") is not True:
+        print("Formato inválido", file=sys.stderr)
+    text_list = list()
+    try:
+        with open(path_file) as file:
+            for line in file:
+                text_list.append(line.strip())
+    except Exception:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    return text_list
