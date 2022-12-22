fcd517e673a6cc9a6d425e4dcf66bf7113badc8e
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-12-22 01:30:08 -0300

    feat: Implementa a função remove e ajusta função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,18 +4,30 @@ import sys
 
 def process(path_file, instance):
     text_lines = txt_importer(path_file)
-    if text_lines not in instance.queue:
-        instance.enqueue(text_lines)
-    processed_data = dict(
-        nome_do_arquivo=path_file,
-        qtd_linhas=len(text_lines),
-        linhas_do_arquivo=text_lines,
-    )
-    print(processed_data, file=sys.stdout)
+    file_exists = False
+    for queue in instance.queue:
+        queue_path = queue["nome_do_arquivo"]
+        if path_file == queue_path:
+            file_exists = True
+            break
+    if file_exists is False:
+        processed_data = dict(
+            nome_do_arquivo=path_file,
+            qtd_linhas=len(text_lines),
+            linhas_do_arquivo=text_lines,
+        )
+        instance.enqueue(processed_data)
+        print(processed_data, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    queue_size = instance.__len__()
+    if queue_size == 0:
+        print("Não há elementos", file=sys.stdout)
+    else:
+        dequeue = instance.dequeue()
+        path_file = dequeue["nome_do_arquivo"]
+        print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
