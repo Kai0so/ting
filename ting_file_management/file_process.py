bb98521069c40d74f70b92d1cabd82e358496f68
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-12-22 00:06:00 -0300

    feat: Implementa a função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text_lines = txt_importer(path_file)
+    if text_lines not in instance.queue:
+        instance.enqueue(text_lines)
+    processed_data = dict(
+        nome_do_arquivo=path_file,
+        qtd_linhas=len(text_lines),
+        linhas_do_arquivo=text_lines,
+    )
+    print(processed_data, file=sys.stdout)
 
 
 def remove(instance):
