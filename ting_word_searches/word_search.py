0b5fc3781f498dab768397b6621cbf4f54600523
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-04 08:38:54 -0300

    feat: Implementa a função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_info = []
+    occurrences = []
+    for queue in instance.queue:
+        file_lines = queue["linhas_do_arquivo"]
+        file_name = queue["nome_do_arquivo"]
+        for line in file_lines:
+            if word.lower() in line.lower():
+                occurrences.append({"linha": file_lines.index(line) + 1})
+        occurrence_result = {
+            "palavra": word,
+            "arquivo": file_name,
+            "ocorrencias": occurrences
+        }
+        search_info.append(occurrence_result)
+    if len(occurrences) < 1:
+        return []
+    return search_info
 
 
 def search_by_word(word, instance):
